d2d146ee4c539a39c78f7f4c04701a7abd736d71
samchat2561 <samchat2561@gmail.com>
2025-01-09 16:34:03 +0700

    update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { registerUser } from '../controllers/authController.js'
+import { loginUser, registerUser } from '../controllers/authController.js'
 
 const router = express.Router()
 
-//http://localhost:3000/api/auth/register
+//1.http://localhost:3000/api/auth/register
 router.post('/register', registerUser)
 
+//2.http://localhost:3000/api/auth/login
+router.post('/login', loginUser)
 export default router
